627f339c85239403e78500beb67cd312ee48356c
Diane <dianekim120@gmail.com>
2022-10-10 20:07:01 -0400

    new app

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createStackNavigator} from 'react-navigation-stack';
+import { StatusBar } from 'expo-status-bar';
+
+const Stack = createNativeStackNavigator();
+// const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="newScreen" component={newScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({navigation}) => {
+  return(
+    <Button 
+      title="Click here to redirect!"
+      onPress={()=>
+        navigation.navigate('newScreen')
+      }
+    
+    />
+  )
+}
+
+const newScreen = ({navigation}) => {
+  return(
+    <Text>hey, you made it!</Text>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <MyStack />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +60,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+
+  }
 });
